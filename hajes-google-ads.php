0b526dfa2826b4d7b339dacec0abb740797d14fc
Haje Jan Kamps <hajejan@kamps.org>
2013-05-04 19:25:09 +0100

    Create version 0.1.0
    First deployment

---
+++ hajes-google-ads.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Plugin Name: Hajes Google Ads
+Plugin URI: http://kamps.org
+Description: Replaces the MORE tag in a Wordpress post with a Google advert at that point.
+Author: Haje Jan Kamps
+Version: 0.1.0
+Author URI: http://www.kamps.org
+*/
+
+$advert_html = '
+
+
+	<!-- Hajes Adsense Inserter Code -->
+	<div class="moreadsense">
+	<script type="text/javascript"><!--
+	google_ad_client = "ca-pub-0188057227511508";
+	/* PC 2013 Banner */
+	google_ad_slot = "3135601498";
+	google_ad_width = 468;
+	google_ad_height = 60;
+	//-->
+	</script>
+	<script type="text/javascript"
+	src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+	</script>
+	</div>
+	
+	
+	';
+
+ 
+// This function replace your more tag with your adsense codes.
+function add_google_ad_at_more($postcontent) {
+	if( is_single() ) :
+		global $advert_html;
+		$pos1 = strpos($postcontent, '<span id="more-');
+		$pos2 = strpos($postcontent, '</span>', $pos1) + 7; // +7 to get rid of the SPAN as well. 
+		$postcontentstart = substr($postcontent, 0, $pos2);
+		$postcontentend = substr($postcontent, $pos2);
+		$text = $postcontentstart .  $advert_html . $postcontentend;
+	endif;
+return $text;
+}
+
+// This function replace your more tag with your adsense codes.
+function add_google_ad_at_end($postcontent) {
+	if( is_single() ) :
+		global $advert_html;
+		$text = $postcontent .  $advert_html;
+	endif;
+return $text;
+}
+
+add_filter('the_content', 'add_google_ad_at_more');
+add_filter('the_content', 'add_google_ad_at_end');
+
+?>
